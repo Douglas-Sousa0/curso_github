c4b7fddb91321aec0a09fafd34feb91fc2b499bd
duug <douglas.sousa16a@gmail.com>
2024-04-23 23:27:22 -0300

    Adicionando funcionalidade para mostrar mensagem

--- cadastro.js
+++ cadastro.js
@@ -3,4 +3,13 @@ function signUp(email){
     return email
 }
 
+
+
 // Pegando o email e cadastrando na plataforma
+
+
+function pegaEmail(email){
+    return 'Bem vindo ao sistema'
+}
+
+// Adicionando função de mensagem
